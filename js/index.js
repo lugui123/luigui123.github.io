a5fe7bfc98930242f748ab757f629a509e5f52fe
luigui123 <fbc_la_ley@hotmail.com>
2022-10-18 21:47:21 -0500

    Creando mi primer sitio web

---
+++ js/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('💞 Now zomos ParTnEr 💞');
+    alert('😱Nuestra VoDa Is MañaNa😱');
+    location.href = 'https://www.youtube.com/watch?v=lOD4tHz4A_8';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  heroTitle.innerHTML += partner + ' ❤';
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
